6cceadfdddf2f112f0f02b0189d8a2867ed88395
apb <apb@apbs-MacBook-Pro.local>
2024-05-29 21:38:07 -0500

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/alexander-berger/go-ps
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os/exec"
+	"strconv"
+	"strings"
+)
+
+type Process struct {
+	User    string
+	PID     int
+	CPU     float64
+	MEM     float64
+	VSZ     int
+	RSS     int
+	TT      string
+	STAT    string
+	Started string
+	Time    string
+	Command string
+}
+
+func main() {
+	processes, err := getProcesses()
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	// filtered := filterByUser(processes, "apb")
+	// filtered := filterByCommand(processes, "/Applications/Obsidian.app/Contents/MacOS/Obsidian")
+	filtered := filterByCommand(processes, "python test/sleep.py")
+
+	for _, process := range filtered {
+		fmt.Printf("%+v\n", process)
+		// process.Kill()
+	}
+}
+
+func (p *Process) Kill() error {
+	cmd := exec.Command("kill", "-9", strconv.Itoa(p.PID))
+
+	err := cmd.Run()
+	return err
+}
+
+func getProcesses() ([]Process, error) {
+	cmd := exec.Command("ps", "aux")
+	output, err := cmd.Output()
+	if err != nil {
+		return nil, err
+	}
+
+	scanner := bufio.NewScanner(strings.NewReader(string(output)))
+	var processes []Process
+
+	// Skip the header line
+	scanner.Scan()
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		fields := strings.Fields(line)
+		if len(fields) < 11 {
+			continue // skip malformed lines
+		}
+
+		pid, _ := strconv.Atoi(fields[1])
+		cpu, _ := strconv.ParseFloat(fields[2], 64)
+		mem, _ := strconv.ParseFloat(fields[3], 64)
+		vsz, _ := strconv.Atoi(fields[4])
+		rss, _ := strconv.Atoi(fields[5])
+
+		process := Process{
+			User:    fields[0],
+			PID:     pid,
+			CPU:     cpu,
+			MEM:     mem,
+			VSZ:     vsz,
+			RSS:     rss,
+			TT:      fields[6],
+			STAT:    fields[7],
+			Started: fields[8],
+			Time:    fields[9],
+			Command: strings.Join(fields[10:], " "), // Command can have spaces
+		}
+
+		processes = append(processes, process)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return processes, nil
+}
+
+// Function to filter the list of processes by user
+func filterByUser(processes []Process, user string) []Process {
+
+	var filtered []Process
+	for _, process := range processes {
+		if process.User == user {
+			filtered = append(filtered, process)
+			// break
+		}
+	}
+	return filtered
+}
+
+// Function to filter the list of processes by command
+func filterByCommand(processes []Process, command string) []Process {
+
+	var filtered []Process
+	for _, process := range processes {
+		if strings.Contains(process.Command, command) {
+			filtered = append(filtered, process)
+			// break
+		}
+	}
+
+	return filtered
+}
